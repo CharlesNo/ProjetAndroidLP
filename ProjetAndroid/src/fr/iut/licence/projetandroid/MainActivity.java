d34a83053b0df0f7b9221e6178a6ec8496b913d2
CharlesNo <charles63500@gmail.com>
2014-01-28 09:30:14 +0100

    Commentaires Main Activity comme des beau goss
    Signed-off-by: CharlesNo <charles63500@gmail.com>

--- ProjetAndroid/src/fr/iut/licence/projetandroid/MainActivity.java
+++ ProjetAndroid/src/fr/iut/licence/projetandroid/MainActivity.java
@@ -1,20 +1,40 @@
 package fr.iut.licence.projetandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
+/**
+ * The Class MainActivity.
+ */
 public class MainActivity extends Activity
 {
+	/* _________________________________________________________ */
+	/**
+	 * On create.
+	 * 
+	 * @param savedInstanceState
+	 *            the saved instance state
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	protected void onCreate(final Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
+	/* _________________________________________________________ */
+	/**
+	 * On create options menu.
+	 * 
+	 * @param menu
+	 *            the menu
+	 * @return true, if successful
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	public boolean onCreateOptionsMenu(final Menu menu)
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
